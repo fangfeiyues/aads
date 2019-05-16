0e41ac247fc6cac02a7b853c4948bbfc3fbde626
wb-ffy394838 <wb-ffy394838@alibaba-inc.com>
2019-05-16 19:25:54 +0800

    binaryTree

--- src/main/java/com/fang/aads/algo/sort/QuickSort.java
+++ src/main/java/com/fang/aads/algo/sort/QuickSort.java
@@ -1,9 +1,7 @@
 package com.fang.aads.algo.sort;
 
 /**
- * 快速排序：把基点拿出留下这个空位
- * 如何在O(n)的时间复杂度内查找到一个无序的数组中的第K大元素？
- * 时间复杂度 O(nlogn) 极端情况下O(n^2)
+ * 快速排序：把基点拿出留下这个空位 如何在O(n)的时间复杂度内查找到一个无序的数组中的第K大元素？ 时间复杂度 O(nlogn) 极端情况下O(n^2)
  */
 public class QuickSort {
     /**
@@ -15,9 +13,7 @@ public class QuickSort {
      */
 
     /**
-     * 分区函数的思想是，在原地排序基础上 取i,j两个浮点，a[p...i-1]都是处理的 后面未处理。
-     * 遍历到比pivot大的i不动 j继续后移；遇到小于的 则交换到i位置同时i,j都后移
-     * 因为涉及到交换操作 所以快拍不稳定
+     * 分区函数1的思想是，在原地排序基础上 取i,j两个浮点，a[p...i-1]都是处理的 后面未处理。 遍历到比pivot大的i不动 j继续后移；遇到小于的 则交换到i位置同时i,j都后移 因为涉及到交换操作 所以快拍不稳定
      *
      * @param a
      * @param p
@@ -29,8 +25,44 @@ public class QuickSort {
         // 拿到分区数
         int pivot = a[r];
 
-
     }
 
+    /**
+     * 分区思想2：取第一个为分区点，i,j不同的作为两段游标不断前移。 i遇到大于p值的则停止，j遇到小于p值的同样停下。同时交换两者
+     *
+     * @param a
+     * @param p 起始节点
+     * @param r 终止节点
+     */
+    private void partition2(int[] a, int p, int r) {
+        int i = p;
+        // r+1 ?? 在--j的时候略过第一位
+        int j = r + 1;
+        while (true) {
+            while (a[++i] > a[p] && i < r) {}
+            while (a[--j] < a[p] && j > p) {}
+            if (i >= j) {
+                break;
+            }
+            swap(a, i, j);
+        }
+        swap(a, p, j);
+    }
+
+    /**
+     * 交换
+     *
+     * @param a
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] a, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
 
 }
--- src/main/java/com/fang/aads/dst/tree/BinaryTreeForeach.java
+++ src/main/java/com/fang/aads/dst/tree/BinaryTreeForeach.java
@@ -3,16 +3,22 @@ package com.fang.aads.dst.tree;
 import com.alibaba.fastjson.JSON;
 
 import javax.swing.tree.TreeNode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
- * created by fang on 2019/4/7/007 19:32
- * 遍历二叉树
+ * created by fang on 2019/4/7/007 19:32 遍历二叉树
  */
 public class BinaryTreeForeach {
 
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 
     /**
      * 非递归中序遍历: 构建栈
@@ -39,7 +45,6 @@ public class BinaryTreeForeach {
 
     }
 
-
     /**
      * 前序遍历
      *
@@ -64,7 +69,6 @@ public class BinaryTreeForeach {
         }
     }
 
-
     /**
      * 后序遍历
      *
@@ -73,28 +77,53 @@ public class BinaryTreeForeach {
      */
     public static List<Integer> aftorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
+        int left = 1;
+        int right = 2;
         Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> point = new Stack<>();
         while (true) {
             if (root != null) {
                 stack.add(root);
                 root = root.left;
+                point.add(left);
             } else {
                 if (stack.isEmpty()) {
                     return list;
                 }
                 root = stack.pop();
-                // 左右孩子都没有的时候加入list
+                // 左右孩子都没有的时候再拿出来加入list
                 if (root.right == null) {
                     list.add(root.val);
                 }
-                root = stack.pop();
                 root = root.right;
-
             }
-
         }
     }
 
+    //public static void postOrderStack(TreeNode root) {
+    //    if (root == null) { return; }
+    //    Stack<TreeNode> s = new Stack<TreeNode>();
+    //    Map<TreeNode, Boolean> map = new HashMap<TreeNode, Boolean>();
+    //    s.push(root);
+    //    while (!s.isEmpty()) {
+    //        TreeNode temp = s.peek();
+    //        if (temp.left != null && !map.containsKey(temp.left)) {
+    //            temp = temp.left;
+    //            while (temp != null) {
+    //                if (map.containsKey(temp)) { break; } else { s.push(temp); }
+    //                temp = temp.left;
+    //            }
+    //            continue;
+    //        }
+    //        if (temp.right != null && !map.containsKey(temp.right)) {
+    //            s.push(temp.right);
+    //            continue;
+    //        }
+    //        TreeNode t = s.pop();
+    //        map.put(t, true);
+    //        System.out.println(t.val);
+    //    }
+    //}
 
     public static void main(String[] args) {
         TreeNode node1 = new TreeNode(10);
@@ -110,17 +139,73 @@ public class BinaryTreeForeach {
         node2.right = node5;
         node3.left = node6;
         node5.right = node7;
-        System.out.println(JSON.toJSONString(aftorderTraversal(node1)));
+        System.out.println(JSON.toJSONString(postorderTraversal2(node1)));
 
     }
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+    // -----------------------------------------------------
 
-        TreeNode(int x) {
-            val = x;
+    public static List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node == null) {
+                continue;
+            }
+            ret.add(node.val);
+            // 先右后左，保证左子树先遍历
+            stack.push(node.right);
+            stack.push(node.left);
         }
+        return ret;
     }
+
+    /**
+     * 1,2d的思想一样的 就是后序的比较方便
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) { return ret; }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            TreeNode node = stack.pop();
+            ret.add(node.val);
+            cur = node.right;
+        }
+        return ret;
+    }
+
+    /**
+     * 前序：root-left-right; 后序：left-right-root 如果把后序改成root-right-left就和前序加入顺序相反
+     *
+     * @param root
+     * @return
+     */
+    public static List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node == null) {
+                continue;
+            }
+            ret.add(node.val);
+            stack.push(node.left);
+            stack.push(node.right);
+        }
+        Collections.reverse(ret);
+        return ret;
+    }
+
 }
