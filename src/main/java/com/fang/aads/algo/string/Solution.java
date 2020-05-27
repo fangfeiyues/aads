7798f2475b849b05be957f5c57802342c37c17bd
fangfeiyue <fangfeiyue@youzan.com>
2020-05-28 00:45:59 +0800

    编码字符串

--- src/main/java/com/fang/aads/algo/string/Solution.java
+++ src/main/java/com/fang/aads/algo/string/Solution.java
@@ -1,5 +1,7 @@
 package com.fang.aads.algo.string;
 
+import java.util.LinkedList;
+
 /**
  * @author created by fang on 2020/5/19/019 23:05
  */
@@ -20,10 +22,47 @@ public class Solution {
             if (s.toCharArray()[i] != s.toCharArray()[length - 1 - i]) {
 
                 if (s.toCharArray()[i + 1] == s.toCharArray()[length - 1 - i]) {
-                    
+
                 }
             }
         }
         return false;
     }
+
+    /**
+     * 给定一个经过编码的字符串，返回它解码后的字符串。
+     * <p>
+     * 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/decode-string
+     *
+     * @param s sd3[qa5[op]ry]
+     * @return
+     */
+    public String decodeString(String s) {
+
+        // 1.栈
+        LinkedList<String> stk = new LinkedList<>();
+        int cur = 0;
+        while (cur < s.length()) {
+            char chars = s.charAt(cur);
+
+            // 字母入栈
+            if (Character.isDefined(chars)) {
+                stk.addLast(chars);
+
+            }
+        }
+
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
 }
