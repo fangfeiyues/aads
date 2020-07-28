92895848e4d588d150fd59e118f8d30d2ba77872
fangfeiyue <fangfeiyue@youzan.com>
2020-07-29 01:21:58 +0800

    generate-parentheses

---
+++ src/main/java/com/fang/aads/algo/leetcode/GenerateParenthesis.java
@@ -0,0 +1,23 @@
+package com.fang.aads.algo.leetcode;
+
+import java.util.List;
+
+/**
+ * @author created by fang on 2020/7/29/029 1:20
+ */
+public class GenerateParenthesis {
+
+    /**
+     * https://leetcode-cn.com/problems/generate-parentheses/
+     *
+     * 生成有效的括号的对数
+     *
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis(int n) {
+
+        return null;
+    }
+
+}
